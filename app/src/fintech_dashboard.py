ad336bdc7cb621faa45933194e915c382ff1a06e
MoTammaa <mohamed.abotammaa@student.guc.edu.eg>
2024-12-14 17:01:30 +0200

    non-interactive questions

--- app/src/fintech_dashboard.py
+++ app/src/fintech_dashboard.py
@@ -3,14 +3,35 @@ import plotly.express as px
 import pandas as pd
 import functions as fn
 
+fintech_df: pd.DataFrame
 fintech_df, lookup = fn.read_data('dashboard')
 
 app = Dash()
 
+# Question 1
 loan_amount_across_grades = px.box(fintech_df, x='letter_grade', y='loan_amount')
-loan_amount_across_grades.update_layout(title={'text':fn.QUESTIONS[0], 'x':0.5, 'xanchor':'center'},
-                                        xaxis_title='Letter Grade', 
-                                        yaxis_title='Loan Amount')
+loan_amount_across_grades.update_layout(xaxis_title='Letter Grade', yaxis_title='Loan Amount')
+
+# Question 2
+
+# Question 3
+
+# Question 4
+avg_loan_amount_states = px.bar(
+    fintech_df.groupby('state')['loan_amount'].mean().reset_index(),
+    x='state', y='loan_amount',
+    color= 'loan_amount',
+    color_continuous_scale=px.colors.sequential.Viridis
+)
+avg_loan_amount_states.update_layout(xaxis_title='State', yaxis_title='Average Loan Amount')
+
+
+# Question 5
+loan_grades_distribution = px.histogram(
+    (fintech_df.value_counts('grade') / fintech_df.shape[0]).reset_index(),
+      x='grade', y='count'
+)
+loan_grades_distribution.update_layout(xaxis_title='Grade', yaxis_title='Percentage Distrb.')
 
 
 
@@ -23,7 +44,30 @@ loan_amount_across_grades.update_layout(title={'text':fn.QUESTIONS[0], 'x':0.5, 
 
 app.layout = html.Div([
     html.H1(children='Fintech Dashboard for Mo Tammaa', style={'textAlign':'center'}, className='title'),
-    dcc.Graph(id='loan_amount-across-grades', figure=loan_amount_across_grades, className='my-graph')
+
+    # Question 1
+    html.Div(children=[
+        html.H3(children=fn.QUESTIONS[0], className='graph-title'),
+        dcc.Graph(id='loan_amount_across_grades', figure=loan_amount_across_grades, className='my-graph')
+    ], className='graph-container', id='graphcont0'),
+
+    # Question 2
+
+    # Question 3
+
+    # Question 4
+    html.Div(children=[
+        html.H3(children=fn.QUESTIONS[3], className='graph-title'),
+        dcc.Graph(id='avg_loan_amount_states', figure=avg_loan_amount_states, className='my-graph')
+    ], className='graph-container', id='graphcont3'),
+
+    # Question 5
+    html.Div(children=[
+        html.H3(children=fn.QUESTIONS[4], className='graph-title'),
+        dcc.Graph(id='loan_grades_distribution', figure=loan_grades_distribution, className='my-graph')
+    ], className='graph-container', id='graphcont4')
+
+
 ], className='container')
 
 if __name__ == '__main__':
